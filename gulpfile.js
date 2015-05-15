817776ad0bbfd8f1fe64e5d0630749c03fe82eae
Luis Dalmolin <luis.nh@gmail.com>
2015-05-15 16:29:10 -0300

    Adicionado gulp livereload - Adicionado exemplo do

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp    = require('gulp');
+var connect = require('gulp-connect');
+var files   = ['Aula1/**/*', 'Aula2/**/*', 'Aula3/**/*', 'Aula4/**/*', 'Aula5/**/*', 'playground.html'];
+
+gulp.task('files', function() {
+  gulp.src(files).pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(files, ['files']);
+});
+
+gulp.task('connect', function() {
+  connect.server({ livereload: true });
+});
+
+gulp.task('default', ['connect', 'watch']);
